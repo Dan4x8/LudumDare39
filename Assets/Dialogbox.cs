8c39564afb38b21554a4b6b14bcd50f1e8383d79
Daniel Schütz <danielschuetz@gmx.ch>
2017-07-29 15:54:39 +0200

    dialog mock up

--- Assets/Dialogbox.cs
+++ Assets/Dialogbox.cs
@@ -1,15 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Dialogbox : MonoBehaviour {
 
+	Image imgTextBackground;
+	Image imgNameBackground;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
+		Image[] imgs = GetComponentsInChildren<Image>();
+		imgTextBackground = imgs[0];
+
+		//SetTransparency(0.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void SetTransparency(float alpha)
+	{
+		imgTextBackground.color = new Color(imgTextBackground.color.r, imgTextBackground.color.g, imgTextBackground.color.b, alpha);
 	}
 }
