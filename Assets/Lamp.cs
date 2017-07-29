2c41acf9a382bb0c8f02a00a6e3a12df62d023bb
Daniel Schütz <danielschuetz@gmx.ch>
2017-07-29 12:03:51 +0200

    third one

---
+++ Assets/Lamp.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lamp : MonoBehaviour
+{
+	private void OnMouseDown()
+	{
+		Debug.Log("clicked");
+	}
+}
