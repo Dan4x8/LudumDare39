6c85d038dcf0baf6d5991005e796000145bc3e80
Daniel Schütz <danielschuetz@gmx.ch>
2017-07-29 18:49:42 +0200

    added clickable rope with message...

---
+++ Assets/RopeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeController : MonoBehaviour
+{
+	public Dialogbox DialogBox;
+
+	private void OnMouseDown()
+	{
+		Dialogbox db = Instantiate(DialogBox, GameObject.FindGameObjectWithTag("DialogCanvas").transform);
+		db.ShowDialog(3);
+	}
+}
