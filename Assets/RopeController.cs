00c917befd051b9fd6163b4eaa9d2bf93aa97723
Daniel Schütz <danielschuetz@gmx.ch>
2017-07-29 20:12:45 +0200

    added fading effect

--- Assets/RopeController.cs
+++ Assets/RopeController.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class RopeController : MonoBehaviour
 {
 	public Dialogbox DialogBox;
+	public SpriteRenderer Whitening;
+
+	public SpriteRenderer Rope;
 
 	private void OnMouseDown()
 	{
+		/*
 		Dialogbox db = Instantiate(DialogBox, GameObject.FindGameObjectWithTag("DialogCanvas").transform);
 		db.ShowDialog(3);
+		*/
+
+		transform.localScale *= 1.1f;
+		Whitening.color *= new Vector4(1f, 1f, 1f, 1.3f);
+		Whitening.transform.localScale *= 1.2f;
+
+		Rope.color *= new Vector4(1.1f, 1.1f, 1.1f, 1f);
 	}
 }
