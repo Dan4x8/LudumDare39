e975865954e8cce7e9d59f0f928c2c99b585846c
Daniel Schütz <danielschuetz@gmx.ch>
2017-07-30 09:16:01 +0200

    experimented with a blob

---
+++ Assets/BlobScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlobScript : MonoBehaviour {
+	public enum BlobDifficulty { ClickOnce}
+
+	public TextMesh Text;
+	public SpriteRenderer Background;
+	public BlobDifficulty Difficulty = BlobDifficulty.ClickOnce;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Background.transform.localScale = new Vector3((Text.text.Length*Text.characterSize*Text.characterSize), 1f, 1f);
+	}
+
+	public void OnMouseDown()
+	{
+		switch(Difficulty)
+		{
+			case BlobDifficulty.ClickOnce:
+				Destroy(transform.parent.gameObject);
+				break;
+			default:
+				break;
+		}
+	}
+}
