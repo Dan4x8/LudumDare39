04aa6003bc32704a88ed886b70ed47f4b0d044cd
Daniel Schütz <danielschuetz@gmx.ch>
2017-07-30 14:34:31 +0200

    added classroom minigame

---
+++ Assets/ClassRoomController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClassRoomController : MonoBehaviour {
+
+	public bool AvoidObjectsMinigame = false;
+	public ThrownSchoolObject ThrownObject;
+
+	// Use this for initialization
+	void Start ()
+	{
+	}
+
+	float timer;
+
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if (AvoidObjectsMinigame)
+		{
+			if (timer <= 0f)
+			{
+				timer = Random.Range(2f, 5f);
+				var obj = Instantiate(ThrownObject);
+				obj.TargetPoint = GameObject.FindGameObjectWithTag("Player").transform.GetChild(0).gameObject;
+			}
+		}
+	}
+}
